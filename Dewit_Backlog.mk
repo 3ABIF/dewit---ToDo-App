f8620c52438e7d6d81fbdf518cc0f0d4751ec65d
steiningerrobin-ux <steininger.robin@gmx.at>
2025-12-11 18:08:12 +0100

    Add files via upload

---
+++ Dewit_Backlog.mk
@@ -0,0 +1,8 @@
+Akzeptanzkriterien
+Der Benutzer muss eineEingabefeld sehen, in dem er den Titel der Aufgabe eingeben kann.
+
+Nach Eingabe und Bestätigung (z.B. durch einen "Hinzufügen"-Button oder Enter) muss die Aufgabe sofort am Anfang oder Ende der Hauptliste erscheinen.
+
+Die neu erstellte Aufgabe muss standardmäßig als "unerledigt" markiert sein.
+
+Die Oberfläche muss dem Benutzer eine visuelle Bestätigung geben, dass die Aufgabe erfolgreich hinzugefügt wurde.
